c14a17fb36ae38e44d425e6a84113560463e8624
DevLomoSE <jonathancggs@gmail.com>
2021-10-17 13:09:58 -0500

    :tada: Init Project
    starting the project for items microservice

---
+++ src/main/java/com/devlomose/app/ms/items/SbItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devlomose.app.ms.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlomose/app/ms/items/SbItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlomose.app.ms.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
